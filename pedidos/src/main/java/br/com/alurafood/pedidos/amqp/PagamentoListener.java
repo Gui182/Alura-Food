a0f15a6f4592a15ffd3bf2647d60c58193983426
Gui182 <guilherme.oliveira182@yahoo.com.br>
2023-03-23 23:39:20 -0300

    termino 22/03

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,14 +1,22 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import br.com.alurafood.pedidos.dto.PagamentoDto;
 
 @Component
 public class PagamentoListener {
     
-    @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMensagem(Message mensagem) {
-        System.out.println("Recebi a mensagem " + mensagem.toString());
+    @RabbitListener(queues = "pagamentos.detalhes-pedidos")
+    public void recebeMensagem(PagamentoDto pagamento) {
+        String mensagem = """
+                Dados do pagamento: %s
+                Numero do pedido %s
+                Valor R$: %s
+                Status: %s
+                """.formatted(pagamento.getId(), pagamento.getPedidoId(), pagamento.getValor(), pagamento.getStatus());
+
+                System.out.println("Recebi a mensagem: " + mensagem);
     }
 }
